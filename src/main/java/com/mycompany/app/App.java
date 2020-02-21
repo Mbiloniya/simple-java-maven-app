2ea8446dbe401313edc9995928f8149eb503c083
Ubuntu <ubuntu@ip-172-31-86-115.ec2.internal>
2020-02-21 06:57:26 +0000

    minor changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Mahesh Biloniya World!";
+    private final String message = "Hello Mahesh Biloniya's World!";
 
     public App() {}
 
